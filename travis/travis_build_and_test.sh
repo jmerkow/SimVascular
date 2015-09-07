22375dc495c3f2f5d5e8e6e88b2c08dd5d72366d
jameson <jmerkow@gmail.com>
2015-09-07 08:44:40 -0700

    explicit call for cmake

--- travis/travis_build_and_test.sh
+++ travis/travis_build_and_test.sh
@@ -3,21 +3,21 @@
 set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
 
-export CMAKE_ARGS=""
-if $PREBUILD_ITKVTK; then
-    CMAKE_ARGS="-DSimVascular_USE_SYSTEM_VTK=1 -DVTK_DIR=$VTK_DIR -DSimVascular_USE_SYSTEM_ITK=1 -DITK_DIR=$ITK_DIR"
-fi
-echo $CMAKE_ARGS
 
 if $WITH_CMAKE; then
-  mkdir $BUILD_DIR
+  mkdir -p $BUILD_DIR
   cd $BUILD_DIR
+    if $PREBUILD_ITKVTK; then
+        cmake -DSimVascular_USE_SYSTEM_VTK:BOOL="1" -DSimVascular_USE_SYSTEM_ITK:BOOL="1" -DVTK_DIR:PATH=$VTK_DIR -DITK_DIR:PATH=$ITK_DIR -DBUILD_ThreeDSolver=1 ../Code
 
-  cmake $CMAKE_ARGS -DBUILD_ThreeDSolver=1 ../Code
-  $MAKE
-  
+    else
+        cmake -DBUILD_ThreeDSolver=1 ../Code
+        
+
+    fi
+
+  $MAKE  
   $MAKE clean
-
   cd -
 
 else
