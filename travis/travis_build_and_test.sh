91851fca09842d83aa0025fc78d88ffd2463b1d1
jameson <jmerkow@gmail.com>
2015-09-06 21:30:42 -0700

    move vtk/itk to cache

--- travis/travis_build_and_test.sh
+++ travis/travis_build_and_test.sh
@@ -3,11 +3,17 @@
 set -e
 MAKE="make --jobs=$NUM_THREADS --keep-going"
 
+export CMAKE_ARGS=""
+if $PREBUILD_ITKVTK; then
+    CMAKE_ARGS="-DSimVascular_USE_SYSTEM_VTK=1 -DVTK_DIR=$VTK_DIR -DSimVascular_USE_SYSTEM_ITK=1 -DITK_DIR=$ITK_DIR"
+fi
+echo $CMAKE_ARGS
+
 if $WITH_CMAKE; then
   mkdir $BUILD_DIR
   cd $BUILD_DIR
 
-  cmake ../Code
+  cmake $CMAKE_ARGS -DBUILD_ThreeDSolver=1 ../Code
   $MAKE
   
   $MAKE clean
