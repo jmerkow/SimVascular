fdd3bb1581f874ecf1e5183e2ce72d6cfc5e9901
Jameson Merkow <jmerkow@gmail.com>
2015-09-09 17:43:10 -0700

    switch to https

--- travis/travis_get_test_data.sh
+++ travis/travis_get_test_data.sh
@@ -4,7 +4,7 @@
 set -e
 
 #this is in here so we can override it if we need too.
-test_clone_args=" git@github.com:SimVascular/SimVascular-Test.git "
+test_clone_args=" https://github.com/SimVascular/SimVascular-Test.git "
 rm -rf $SV_TEST_DIR
 git clone $test_clone_args $SV_TEST_DIR
 
